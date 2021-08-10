09a2c68b557bfe89ea5944acc54d6b781fa68962
traplordhuey <hupaulcamacho03@gmail.com>
2021-08-10 19:54:28 -0400

    create import file to declutter main components and load images on the game page

--- client/src/Components/GuiltyGear.js
+++ client/src/Components/GuiltyGear.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import '../CSS/GuiltyGear.css';
+import { characters } from '../Loaders/GG_loader';
 
 export default function GuiltyGear() {
+    let charComponents = [];
+
+    for (let char in characters) {
+        charComponents.push(
+            <div className='char'>
+                <img className='char-icon'src={characters[char].icon} height='100px'/><br/>
+                <p>{characters[char].charName}</p>
+            </div>
+        )
+    }
     return (
         <div>
-            <h1>Guilty Gear</h1>
+            <h1>Guilty Gear: Strive</h1>
+            <div className='characters'>
+                {charComponents}
+            </div>
         </div>
     )
 }
---
+++ client/src/Loaders/GG_loader.js
@@ -0,0 +1,25 @@
+// asset import
+import solIcon from "../Assets/CharIcons/sol.jpg";
+import solPortrait from "../Assets/CharPortraits/GGST_SOL.png";
+
+import kyIcon from "../Assets/CharIcons/ky.jpg";
+import kyPortrait from "../Assets/CharPortraits/GGST_KY.png";
+
+
+// object definition
+export let characters = {
+    sol: {
+        charName: "Sol Badguy",
+        icon: solIcon,
+        portrait: solPortrait,
+        type: "Balanced"
+    },
+    ky: {
+        charName: "Ky Kiske",
+        icon: kyIcon,
+        portrait: kyPortrait,
+        type: "Balanced"
+    }
+    
+}
+
